d0de828ca0cadbdc75321fdb04344470f4999e20
nicomizono <nmizono@gmail.com>
2016-01-17 18:52:46 -0800

    hw 3

---
+++ In_class_5/Part_1/part_1.js
@@ -0,0 +1,11 @@
+prompt ("What was your best marathon time?");
+
+if (var=)
+
+// for (var i=4; i<5; i++){
+//   prompt("Average");
+// }
+//
+// for (var i=2; i<5; i++){
+//   prompt("Excellent");
+// }
