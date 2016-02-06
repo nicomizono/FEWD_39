638b955a31d8792c5d23db49f7615b0cac386cd3
nicomizono <nmizono@gmail.com>
2016-02-06 09:57:49 -0800

    Jquery adding animations

--- Homework_6/main.js
+++ Homework_6/main.js
@@ -1,3 +1,28 @@
-$(window).load(function() {
-   $("main-content").fadeIn(100, function(){});
+$('body').css('display', 'none').fadeIn('slow');
+
+$('p').on('mouseover', function(){
+  $('p').css('background','yellow');
+});
+
+$('p').on('mouseout', function(){
+  $('p').css('background','none');
+});
+
+$('h2').on('click', function(){
+  $('h2').css('margin-left','20px');
+  $('h2').fadeTo('400','0.25', function(){
+    $('.speech').fadeTo('400','0.50');
+  });
+});
+
+$('#switcher-default').on('click', function(){
+  $('.speech').css('font-size','1.0em');
+});
+
+$('#switcher-large').on('click', function(){
+  $('.speech').css('font-size','1.5em');
+});
+
+$('#switcher-small').on('click', function(){
+  $('.speech').css('font-size','0.75em');
 });
