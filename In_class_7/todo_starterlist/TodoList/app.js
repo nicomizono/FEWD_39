d073d886f593feb639eece0a634e0e72abd94d9f
nicomizono <nmizono@gmail.com>
2016-02-10 21:38:33 -0800

    final project

--- In_class_7/todo_starterlist/TodoList/app.js
+++ In_class_7/todo_starterlist/TodoList/app.js
@@ -1,3 +1,58 @@
+//Setting up & grabbing things
+
+var form = document.querySelector("#new-todo-item-form");
+var item = document.querySelector("#new-todo-item-input");
+var parent = document.querySelector("#todo-list");
+
+//add items to list
+
+function addListItem(todo){//when you define a function, you can create any internal variable i.e. todo to use within the funciton.
+  var listItem = document.createElement("li");
+  listItem.textContent = todo;
+  parent.appendChild(listItem);
+}
+
+function formSubmitted(buttonClicked){
+  buttonClicked.preventDefault(); //on any event that comes in, prevent the default.
+  addListItem(input.value);//calling the function, adding the value that was captured in the previous function above. When you call the function, you need to use the real stuff.
+  form.reset();
+}
+
+form.addEventListener("submit", formSubmitted);//call back function = is invoked after some kind of event so formSubmitted does not need ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // var form = document.querySelector("#new-todo-item-form");
 // var input = document.querySelector("#new-todo-item-input");
 // var list = document.querySelector("#todo-list");
@@ -23,16 +78,15 @@
 // form.addEventListener("submit", formSubmitted);
 
 // jQuery
-var $form = $("#new-todo-item-form");
-var $input = $("#new-todo-item-input");
-var $list = $("#todo-list");
-
-$form.on("submit", formSubmitted);
-
-function formSubmitted(event){
-  event.preventDefault();
-  var newListItem = "<li>" + $input.val() + "</li>";
-  $list.append(newListItem);
-  $form[0].reset();
-}
-
+// var $form = $("#new-todo-item-form");
+// var $input = $("#new-todo-item-input");
+// var $list = $("#todo-list");
+//
+// $form.on("submit", formSubmitted);
+//
+// function formSubmitted(event){
+//   event.preventDefault();
+//   var newListItem = "<li>" + $input.val() + "</li>";
+//   $list.append(newListItem);
+//   $form[0].reset();
+// }
